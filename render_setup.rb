fd98b2a4c99f5c13793c2f5662583eba69f895e9
Ed Robinson <edward-robinson@cookpad.com>
2018-02-26 19:56:28 +0000

    Clean up output slightly

--- render_setup.rb
+++ render_setup.rb
@@ -15,7 +15,7 @@ HEREDOC
   "K8sMaster" => %w[master ecr cni autoscaler loadbalancing],
   "K8sNode"   => %w[node ecr cni],
 }.each do |role, policies|
-  SCRIPT << "aws iam create-role --role-name #{role} --assume-role-policy-document #{compact_json("ec2-role-trust-policy.json")}\n"
+  SCRIPT << "aws iam create-role --role-name #{role} --assume-role-policy-document #{compact_json("ec2-role-trust-policy.json")} > /dev/null\n"
   policies.each do |policy|
     SCRIPT << "aws iam put-role-policy --role-name #{role} --policy-name #{policy} --policy-document #{compact_json("#{policy}.json")}\n"
   end
